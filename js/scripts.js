92f90e51870aad1cc92339956c9d115eaabd4872
Aurora Shido-Wagenet <fullmetalwoman@gmail.com>
2019-01-24 12:31:12 -0800

    add final functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,26 @@
-$(function(){
-  $("form#leap-year").submit(function(event){
-    var leapYear = function(year){
-      if (year % 4 === 0){
-        return true;
-      } else {
-        return false;
-      }
+  var leapYear = function(year){
+    if ((year % 100 !==0) && (year % 4 === 0) || (year % 400 === 0)){
+      return true;
+    } else {
+      return false;
     }
+  }
 
+$(function(){
+
+  $("form#leap-year").submit(function(event){
     event.preventDefault();
     var year = parseInt($("input#year").val());
     var result = leapYear(year);
-    $("#result").text(result);
+
+    $(".year").text(year);
+
+    if (!result) {
+      $(".not").text("not");
+    } else {
+      $(".not").text("");
+    }
+    
+    $("#result").show();
   });
 });
